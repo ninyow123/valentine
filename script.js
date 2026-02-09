c5155660debb74834459e94b0e90bdb0e134521d
ninyow123 <dionpisan01623@gmail.com>
2026-02-09 22:23:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+// When "Yes" is clicked, change the text and the GIF
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "I love you too! ❤️";
+    gif.src = "https://media.giphy.com/media/UMon0FO7oGdLq/giphy.gif"; // Happy GIF
+});
+
+// When hovering over "No", make it move to a random position
+noBtn.addEventListener("mouseover", () => {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.style.position = "absolute";
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+});
